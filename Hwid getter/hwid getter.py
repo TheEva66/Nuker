785ead362ee9e7b147cb2aea50dc0cb222aba1f9
TheEva66 <104537575+TheEva66@users.noreply.github.com>
2022-05-20 13:27:42 -0400

    reeee

--- Hwid getter/hwid getter.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import time
-import os
-hwid = subprocess.check_output('wmic csproduct get uuid').decode().split('\n')[1].strip()
-f= open("HWID.txt","w+")
-f.write(hwid)
-f.close
-print("HWID printed to HWID.txt")
-time.sleep(10)
